ad90fbec1474f39a821243a8942081b5c71e3666
nade-harlow <omonadefranklyn@gmail.com>
2021-10-05 14:40:05 +0100

    application struct removed

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -4,31 +4,27 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/nade-harlow/WeekEightTask/Village-square/controllers/handlers"
-	"github.com/nade-harlow/WeekEightTask/Village-square/models"
 	"github.com/nade-harlow/WeekEightTask/Village-square/pkg/database"
 	"log"
 )
 
-type Application struct {
-	blogs *models.DbModel
-}
-
 func main() {
-	db, err:= database.MySqlCon()
+	db, err := database.MySqlCon()
 	defer db.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r:= gin.Default()
-	//r.LoadHTMLGlob("./ui/html/*")
+	r := gin.Default()
+	r.LoadHTMLGlob("./ui/html/*")
 
 	r.GET("/", handlers.SignUp)
 	r.POST("/form", handlers.SignUpForm)
 	r.GET("/login", handlers.Login)
 	r.POST("/login/form", handlers.LoginForm)
+	r.POST("/add", handlers.AddPost)
 	er := r.Run()
 	if er != nil {
-		return 
+		return
 	}
 }
