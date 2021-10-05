22d6a1c5d37d1a0fb4454b395a2b5515a018de79
nade-harlow <omonadefranklyn@gmail.com>
2021-10-05 14:38:57 +0100

    add post

--- controllers/handlers/handler.go
+++ controllers/handlers/handler.go
@@ -2,14 +2,15 @@ package handlers
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/nade-harlow/WeekEightTask/Village-square/controllers/helper"
 	"github.com/nade-harlow/WeekEightTask/Village-square/models"
 	"github.com/nade-harlow/WeekEightTask/Village-square/pkg/database"
 	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
-	"regexp"
 	"time"
 )
 
@@ -20,94 +21,166 @@ func init() {
 	Db, _ = database.MySqlCon()
 }
 
-func SignUp(c *gin.Context)  {
-	c.HTML(http.StatusOK,"signup.html\n",nil)
+func SignUp(c *gin.Context) {
+	c.HTML(http.StatusOK, "signup.html", nil)
 }
 
-func isEmailValid(e string, c *gin.Context)  string {
-	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
-	if emailRegex.MatchString(e)== false{
-		c.String(400, "Invalid email\n")
-		c.Abort()
-		//c.AbortWithStatus(400)
-	}
-	return e
-}
+func SignUpForm(c *gin.Context) {
 
-func SignUpForm(c *gin.Context)  {
-	err := c.BindJSON(&user)
-	if err != nil{
-		log.Println(err)
+	fname := c.PostForm("fname")
+	lname := c.PostForm("lname")
+	email := c.PostForm("email")
+	pword := c.PostForm("pword")
+	rpword := c.PostForm("rpword")
+	helper.VerifyEmpty(fname, lname, c)
+	//if fname == "" {
+	//	c.String(400, "Invalid first name")
+	//	return
+	//}
+	//if lname == "" {
+	//	c.String(400, "Invalid last name")
+	//	return
+	//}
+
+	VerfiedEmail := helper.IsEmailValid(email, c)
+
+	var v models.User
+	Db.QueryRow("SELECT FROM users WHERE email = ?", VerfiedEmail).Scan(&v.UserId, v.FirstName, v.LastName, v.Email, v.Password, v.TimeCreated)
+	if VerfiedEmail == v.Email {
+		c.String(400, "Email already exist")
 		return
-	}
-
-	if user.FirstName == "" {
-		c.String(400, "Invalid first name\n")
-	}
-	if user.LastName == "" {
-		c.String(400, "Invalid last name\n")
-	}
-
-	VerfiedEmail:=isEmailValid(user.Email, c)
-
-	log.Println(user.FirstName,user.LastName,user.Email,user.Password)
-
-	if len(user.Password) < 6 || len(user.ConfrimPassword)<6{
-		c.JSON(400, gin.H{"message":"password must contain at least six characters!"})
-		c.Abort()
+	} ////models.DbModel.SignUpHandler(user)
+	if len(pword) < 6 || len(rpword) < 6 {
+		c.String(400, "password must contain at least six characters!")
+		return
 		//c.String(400, "password must contain at least six characters!\n")
 	}
-	if user.Password == user.ConfrimPassword{
+	if pword != rpword {
 		c.String(400, "password mismatched\n")
-	}else {
-		hashedPassword, err :=bcrypt.GenerateFromPassword([]byte(user.Password),bcrypt.MinCost)
-		users:= &models.User{
-			UserId:    uuid.New().String(),
-			FirstName: user.FirstName,
-			LastName:  user.LastName,
-			Email:     VerfiedEmail,
-			Password: string(hashedPassword),
+		return
+	} else {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(pword), bcrypt.MinCost)
+		users := &models.User{
+			UserId:      uuid.New().String(),
+			FirstName:   fname,
+			LastName:    lname,
+			Email:       email,
+			Password:    string(hashedPassword),
 			TimeCreated: time.Now().Format(time.RFC850),
 		}
-
-
-		////models.DbModel.SignUpHandler(user)
-		//
-		////log.Println(string(hashedPassword))
 
 		stmt, err := Db.Prepare("INSERT INTO users (id, first_name, last_name, email, password, time_created) VALUE (?,?,?,?,?,?)")
 		defer stmt.Close()
 		if err != nil {
-			log.Fatal("unable to insert data ", err)
+			log.Println(err.Error())
+			return
 		}
 		_, err = stmt.Exec(users.UserId, users.FirstName, users.LastName, users.Email, users.Password, users.TimeCreated)
 		//log.Println(user.TimeCreated)
 		if err != nil {
-			log.Fatal("unable to insert data ", err)
+			log.Println(err.Error())
+			c.String(400, "unable to insert data")
+			return
 		}
 	}
-
+	c.String(200, "signup successfully")
 
 }
 
-func Login(c *gin.Context)  {
-	c.HTML(http.StatusOK,"login.html",nil)
+func Login(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
 }
 
-func LoginForm(c *gin.Context)  {
-	email:= c.PostForm("email")
-	password:= c.PostForm("pword")
+func LoginForm(c *gin.Context) {
+	email := c.PostForm("email")
+	pword := c.PostForm("pword")
 
 	//dbM := models.DbModel{}
-	//dbM.LoginHandler(email, password)
+	//dbM.LoginHandler(user.Email, user.Password, c)
 
-	Db.QueryRow("select * from users where first_name= ?",email ).Scan(&user.UserId,&user.FirstName,&user.LastName,&user.Email,&user.Password,&user.TimeCreated)
+	Db.QueryRow("select * from users where email= ?", email).Scan(&user.UserId, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.TimeCreated)
 
-	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-	if err != nil{
-		log.Fatal("wrong username and password", err)
-		c.Status(http.StatusNotAcceptable)
+	er := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pword))
+	log.Println(user.Password, pword)
+	if er != nil {
+		log.Println("wrong username and password: ", er)
+		c.String(406, "wrong username and password")
+		return
 	}
 
-	c.Status(http.StatusFound)
+	c.String(200, "Login successful!")
+}
+
+func AddPost(c *gin.Context) {
+	var post models.Post
+	err := c.BindJSON(&post)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	stmt, err := Db.Prepare(fmt.Sprintf("INSERT INTO posts(id, title, boby, time_created, user_id) VALUES(?, ?, ?, ?)"))
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	post.Id = "ad01eefe-aaad-45b1-b5ce-2a2b1e53b92c"
+	post.TimeCreated = time.Now().Format(time.RFC850)
+
+	res, err := stmt.Exec(post.Id, post.Title, post.Body, post.TimeCreated)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	total, _ := res.RowsAffected()
+	if total < 1 {
+		log.Println(err.Error())
+		return
+	}
+	c.JSON(200, gin.H{"message": "Okay"})
+	//if post.Title == "" {
+	//	c.JSON(400, log.Println(err.Error())
+	//		returngin.H{"message":"enter title fields"})
+	//	//c.Redirect(302, "...")
+	//}
+	//if post.Body == "" {
+	//	//http.Error(w, "Enter Content field", 301)
+	//	c.JSON(400, gin.H{"message":"enter body field"})
+	//	//c.Redirect(302, "...")
+	//} else {
+	//	log.Println(post.Body,post.Title)
+	//	add := models.Post{
+	//		Id:      uuid.New().String(),
+	//		Title:   post.Title,
+	//		Body: 	 post.Body,
+	//		TimeCreated:    time.Now().Format(time.RFC850),
+	//	}
+	//	//
+	//	//statement:= `INSERT INTO posts (id, title, body, time_created) VALUE (?,?,?,?)`
+	//	//
+	//
+	//	stmt, er := Db.Prepare("INSERT INTO posts (id, title, body, time_created) VALUES (?,?,?,?)")
+	//	if er != nil{
+	//		log.Println(err)
+	//		c.JSON(500, gin.H{"message": "can't insert post to database "})
+	//		return
+	//	}
+	//
+	//	//defer stmt.Close()
+	//
+	//	fmt.Println("copied")
+	//	_, errr := stmt.Exec(add.Id, add.Title, add.Body, add.TimeCreated)
+	//	fmt.Println("i got here")
+	//
+	//	if errr != nil {
+	//		log.Println("unable to insert data ", er)
+	//		c.JSON(500, gin.H{"message":"unable to insert data"})
+	//
+	//	}else {
+	//
+	//		c.JSON(200, gin.H{"message":"post uploaded successfully"})
+	//	}
+
+	//c.Redirect(302,"")
+
 }
