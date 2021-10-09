57518408f45c6480b5f81eb9281e54f1d056fd4a
nade-harlow <omonadefranklyn@gmail.com>
2021-10-09 16:45:59 +0100

    comment struct field

--- models/comment.go
+++ models/comment.go
@@ -1,9 +1,17 @@
 package models
 
+import (
+	"database/sql"
+)
+
 type Comment struct {
 	Id          string `json:"id"`
 	Comments    string `json:"comments"`
 	UserId      string `json:"user_id"`
 	PostId      string `json:"post_id"`
 	TimeCreated string `json:"time_created"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
 }
+
+var Db *sql.DB
