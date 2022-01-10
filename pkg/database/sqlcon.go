f6696cf28537de63fb21c18d88919ffd250e72fd
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2022-01-10 21:05:51 +0100

    bug fix

--- pkg/database/sqlcon.go
+++ pkg/database/sqlcon.go
@@ -16,9 +16,7 @@ func MySqlCon() (db *sql.DB, err error) {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
