506abdf4890263f6769f7ea1fd7841ebd20a355b
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2022-01-10 20:22:35 +0100

    environmental variables introduced

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"github.com/nade-harlow/WeekEightTask/Village-square/pkg/database"
 	"log"
+	"os"
 )
 
 func main() {
+	er := godotenv.Load()
+	if er != nil {
+		log.Println(er.Error())
+	}
+	port := ":" + os.Getenv("PORT")
+	if port == ":" {
+		port += "8080"
+	}
 	db, err := database.MySqlCon()
 	defer db.Close()
 	if err != nil {
@@ -16,7 +26,7 @@ func main() {
 	r := gin.Default()
 	Routes(r)
 	r.LoadHTMLGlob("./ui/html/*")
-	er := r.Run()
+	er = r.Run(port)
 	if er != nil {
 		return
 	}
