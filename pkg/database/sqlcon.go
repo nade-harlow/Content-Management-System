e15ee671b86ad750c0d6bb84013517369c46edd8
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2022-01-25 18:11:27 +0100

    sql migration

--- pkg/database/sqlcon.go
+++ pkg/database/sqlcon.go
@@ -10,15 +10,15 @@ import (
 
 func MySqlCon() (db *sql.DB, err error) {
 
-	host := os.Getenv("HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	//host := os.Getenv("HOST")
+	//port := os.Getenv("DB_PORT")
+	//user := os.Getenv("DB_USER")
+	//password := os.Getenv("DB_PASSWORD")
+	//dbname := os.Getenv("DB_NAME")
+	//
+	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	db, err = sql.Open("postgres", psqlInfo)
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		return nil, err
 	}
