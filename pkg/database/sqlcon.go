f54af7a74198075f8f7b61155ac516f8b20965b3
Franklyn Omonade <77660863+nade-harlow@users.noreply.github.com>
2022-01-29 21:48:24 +0100

    db url refactored

--- pkg/database/sqlcon.go
+++ pkg/database/sqlcon.go
@@ -16,7 +16,7 @@ func MySqlCon() (db *sql.DB, err error) {
 	//
 	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-	db, err = sql.Open("postgres", "postgres://zcxydxjqbdeeqe:4d3c5682ea4de184ab2e59f26b5ff9981196ab729e615da5a8d91056198f5c4c@ec2-63-32-7-190.eu-west-1.compute.amazonaws.com:5432/dch1l4n831j80m")
+	db, err = sql.Open("postgres", "postgres://etfnhypcroxzcr:e1e63cc4807e17d065ce407470611f7861a32be49fd52754d3428ec9c5cb09b9@ec2-18-203-64-130.eu-west-1.compute.amazonaws.com:5432/d453tbtrt69gc8")
 	if err != nil {
 		return nil, err
 	}
